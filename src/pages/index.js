d4c8c9215481b0b0bdf2bd042998d028b27904e0
Luis Salas <luisalas@gmail.com>
2019-12-16 17:12:24 -0500

    cambio de texto primera sección

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,17 +15,15 @@ export default () => (
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
-          </h1>
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">Producto XYZ</h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Free landing page template to promote your business startup and generate leads for the
-            offered services
+            Landing Page del Producto XYZ. Ahora es más fácil promover los productos de una manera
+            simple y rápida utilizando React!
           </p>
           <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
+            <Button size="lg">Solicita Aquí</Button>
           </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
+          <p className="mt-4 text-gray-600">En menos de 90 segundos</p>
         </div>
         <div className="lg:w-1/2">
           <HeroImage />
